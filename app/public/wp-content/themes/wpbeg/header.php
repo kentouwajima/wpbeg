d7207e10a9918abd6765ebc8b30fde1b04ed031c
kentouwajima <u.kento6366@gmail.com>
2025-04-23 15:36:34 +0900

    head部分の編集

--- app/public/wp-content/themes/wpbeg/header.php
+++ app/public/wp-content/themes/wpbeg/header.php
@@ -1,18 +1,12 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="<?php language_attributes(); ?>">
 	<head>
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="format-detection" content="telephone=no">
-		<title>index | wpbeg-static</title>
 		<meta name="description" content="WordPress theme development static data for beginners">
 		<meta name="keywords" content="WordPress, Theme, development">
-		<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/earlyaccess/mplus1p.css">
-		<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Sacramento&amp;amp;subset=latin-ext">
-		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css">
-		<link rel="stylesheet" href="css/wpbeg.css">
 		<link rel="shortcut icon" href="//wp.yat-net.com/wpbeg-static/images/common/ico/favicon.ico">
 	</head>
 	<body>
